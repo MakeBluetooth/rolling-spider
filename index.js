80f56c80a0be161238452b5654b686a788fe8598
Sandeep Mistry <sandeep.mistry@gmail.com>
2015-06-09 21:11:39 -0400

    flat trim, take off, then land

--- index.js
+++ index.js
@@ -10,8 +10,24 @@ rs.connect(function(error) {
 
   console.log('Rolling Spider connected successfully, setting up ...');
   rs.setup(function() {
-    console.log('Rolling spider setup successfully, starting ping ...');
+    console.log('Rolling Spider setup successfully, flat trim + starting ping ...');
+    rs.flatTrim();
     rs.startPing();
+    rs.flatTrim();
 
+    setTimeout(function() {
+      console.log('Rolling Spider take off!');
+      rs.takeOff(function() {
+        console.log('Rolling Spider in air ...');
+
+        setTimeout(function() {
+          console.log('Rolling Spider land!');
+          rs.land(function() {
+            console.log('Rolling Spider landed ... bye');
+            process.exit(0);
+          });
+        }, 2000);
+      });
+    }, 1000);
   });
 });
