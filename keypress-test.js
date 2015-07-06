2704f33e5d75a8edbb3fac5c28f2dd605bab84a7
Sandeep Mistry <sandeep.mistry@gmail.com>
2015-07-06 17:03:36 -0400

    debouncing keyboard input

--- keypress-test.js
+++ keypress-test.js
@@ -2,12 +2,25 @@ var keypress = require('keypress');
 
 keypress(process.stdin);
 
+var active = false;
+
 process.stdin.setRawMode(true);
 
+function clearActive() {
+  active = false;
+}
+
 process.stdin.on('keypress', function (ch, key) {
+  if (active) {
+    return;
+  }
+  active = true;
+
   console.log('got "keypress"', key.name);
 
   if (key.ctrl && key.name == 'c') {
     process.exit();
   }
+
+  setTimeout(clearActive, 100);
 });
