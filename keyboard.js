8733734bd4670d8e3266f30347a6bf8dceb6dbbf
Sandeep Mistry <sandeep.mistry@gmail.com>
2015-07-06 19:57:23 -0400

    keyboard motion mapping\!

--- keyboard.js
+++ keyboard.js
@@ -55,12 +55,40 @@ process.stdin.on('keypress', function (ch, key) {
   }
   active = true;
 
+  var options = {
+    steps: 2
+  };
+
   if (key.name === 't') {
     console.log('taking off');
     rs.takeOff();
   } else if (key.name === 'l') {
     console.log('landing');
     rs.land();
+  } else if (key.name === 'up') {
+    console.log('up');
+    rs.up(options);
+  } else if (key.name ==='down') {
+    console.log('down');
+    rs.down(options);
+  } else if (key.name === 'left') {
+    console.log('turn left');
+    rs.turnLeft(options);
+  } else if (key.name ==='right') {
+    console.log('turn right');
+    rs.turnRight(options);
+  } else if (key.name === 'w') {
+    console.log('forward');
+    rs.forward(options);
+  } else if (key.name ==='s') {
+    console.log('backward');
+    rs.backward(options);
+  } else if (key.name === 'a') {
+    console.log('left');
+    rs.left(options);
+  } else if (key.name ==='d') {
+    console.log('right');
+    rs.right(options);
   }
 
   setTimeout(clearActive, 100);
